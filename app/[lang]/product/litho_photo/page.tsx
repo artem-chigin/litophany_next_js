e95a6cba094bc95771252a5e30e91656f89c30b4
artem-chigin <chiartftc@gmail.com>
2023-09-06 15:51:21 +0400

    created carousel and product form

---
+++ app/[lang]/product/litho_photo/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Carousel from "@/app/components/Carousel"
+
+let imagePaths = ["/products/litho-photo/photo1.jpg", "/products/litho-photo/photo2.jpg"];
+
+
+export default function Product() {
+    return (
+        <section className="py-10 max-w-screen-xl m-auto border">
+            <h3 className="py-3 text-center text-5xl text-gray-800">Litho Photo</h3>
+            <div className="lg:flex justify-evenly">
+                <div className="block lg:w-1/2 px-5">
+                    <Carousel paths_to_pictures={imagePaths}/>
+                </div>
+                <p className="p-10 lg:w-1/2 border">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+            </div>
+
+                
+        </section>
+    )
+}
--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,27 +1,37 @@
+'use client'
+ 
+import { useState } from 'react'
 import Image from "next/image"
 
-export default function Carousel() {
-    return (
-    <div>
+interface Pictures {
+	paths_to_pictures: string[];
+}
 
-      <div>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+export default function Carousel({ paths_to_pictures } : Pictures) {
+	const imagesLength = paths_to_pictures.length;
+
+	const [index, setIndex] = useState(0)
+
+    return (
+    <div className="relative" onClick={() => setIndex((index === 0) ? imagesLength - 1: index -1)}>
+
+      <div className="absolute left-0 top-1/2 hover:text-gray-100">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
 					<path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
 				</svg>
       </div>
-
 			<div>
 				<Image
-							className='justify-self-start'
-							src={""}
-							width={200}
-							height={200}
+							className=''
+							src={paths_to_pictures[index]}
+							width={640}
+							height={640}
 							alt="Litho"
 						/>
 			</div>
 
-			<div>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+			<div className="absolute right-0 top-1/2 hover:text-gray-100" onClick={() => setIndex((index + 1) % imagesLength)}>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
 					<path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
 				</svg>
 			</div>
