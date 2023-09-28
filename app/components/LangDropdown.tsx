4f183d8ad3d6997759eaf402a8beed0c9b3bd5e8
artem-chigin <chiartftc@gmail.com>
2023-09-28 14:28:00 +0400

    added clickOutsideComponent to navBar

--- app/components/LangDropdown.tsx
+++ app/components/LangDropdown.tsx
@@ -6,18 +6,18 @@ import Link from "next/dist/client/link";
 
 
 export default function LangDropdown( {lang}: {lang: Locale}) {
-    const [visible, setVisible] = useState("hidden");
+    const [dropdownState, setDropdownState] = useState("hidden");
 
     return (
         <div className="inline">
-            <div className="flex items-center  hover:text-slate-800 hover:bg-slate-500"  onClick={() => setVisible((visible === "visible") ? "hidden": "visible")} >
+            <div className="flex items-center  hover:text-slate-800 hover:bg-slate-500"  onClick={() => setDropdownState((dropdownState === "visible") ? "hidden": "visible")} >
                 <button className="inline px-6 py-3">{lang}</button>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
             </div>
 
-        <div id="myDropdown" className={`${visible} absolute border`}>
+        <div id="myDropdown" className={`${dropdownState} absolute border`}>
           <ul>
             <li>
                 <Link href="#">English</Link>
--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import Link from "next/link";
-import { use, useState } from "react";
+import { use, useState, MouseEvent, FocusEvent } from "react";
+import { useOutsideClick } from "../click_outside";
 
 import LangDropdown from "./LangDropdown";
 import { Locale } from "@/i18n-config";
@@ -15,8 +16,14 @@ interface bar {
 export default function NavMenu( {lang, menu} : {lang: Locale, menu: bar}) {
     // const menu: bar = navBar[lang];
     // console.log(typeof(data))
+    const [menuState, setMenuState] = useState("hidden");
 
-    const [menuType, setmenuType] = useState("hidden");
+    const ref = useOutsideClick(() => {
+        setMenuState("hidden")
+        console.log('Clicked outside of MyComponent');
+      });
+
+
     return (
         <nav className="flex items-center">
 
@@ -36,7 +43,7 @@ export default function NavMenu( {lang, menu} : {lang: Locale, menu: bar}) {
             </div>
         
             <div className="visible lg:invisible">
-                <div className="flex justify-end" onClick={() => setmenuType((menuType === "visible") ? "hidden": "visible")}>
+                <div ref={ref} className="dropdown-btn flex justify-end" onClick={() => setMenuState((menuState === "visible") ? "hidden": "visible")}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
@@ -44,8 +51,8 @@ export default function NavMenu( {lang, menu} : {lang: Locale, menu: bar}) {
                 
                 {/* <hr></hr> */}
 
-                <div className={`${menuType} my-5 absolute right-0`}>
-                    <ul className="text-end lg:hidden">
+                <div className={`dropdown-menu ${menuState} my-5 absolute right-0 lg:hidden`}>
+                    <ul className="text-end">
                         <li className="nav-element">
                         <Link href={`/${lang}/`}>{menu.home}</Link>
                         </li>
